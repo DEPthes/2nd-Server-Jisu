c0cb667172eaa2f750476a2ea942c3b168d68595
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-06-21 16:41:26 +0900

    JPA 환경세팅

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] arg) {
+        // JPA 구동 방식 이미지를 따라 Persistence -> EntityManagerFactory 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();   // 트랜잭션 얻어오기
+        tx.begin();                                   // 트랜잭션 시작
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member);
+
+        tx.commit();                                  // 트랜잭션 커밋
+
+        em.close();
+        // 애플리케이션이 완전히 종료되면 EntityMangerFactory close
+        emf.close();
+    }
+}
