1a5b0283a1ddd5c97c694aa27f21127687dc0d1e
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-07-07 04:21:56 +0900

    feat: Controller 정의 및 요구 기능 주석 처리

---
+++ study-board/src/main/java/com/example/studyboard/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.studyboard.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+    // 게시글 작성
+    // 게시글 조회
+    // 게시글 수정
+    // 게시글 삭제
+}
---
+++ study-board/src/main/java/com/example/studyboard/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.studyboard.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    // 회원가입
+    // 로그인
+    // 아이디 중복체크
+
+    // (ROLE: ADMIN) 게시글 삭제
+
+    // 내 정보 조회
+    // 내 정보 수정
+    // 내가 쓴 게시글 확인
+
+    // 비밀번호 변경
+
+}
